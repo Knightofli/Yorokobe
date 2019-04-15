aadd7bca5283490135c5ad9eb5500afe7a025dc3
Knightofli <stevenlidu92@gmail.com>
2019-04-15 22:01:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const Discord = require('discord.js');
 
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+    msg.reply('Pong!');
+}
+});
+
+bot.login(process.env.TOKEN);
