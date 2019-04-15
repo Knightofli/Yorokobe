62d907b64c0f4963d8a0993b73d37ada3432e075
Knightofli <stevenlidu92@gmail.com>
2019-04-15 23:19:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,14 @@ client.on('message', msg => {
 }
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
 client.login(process.env.TOKEN);
